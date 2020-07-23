7a2bd0646c2e4b2f5dfddf80ebcfcd388d1ff8a9
Amit Upadhyay <amitu2016@gmail.com>
2020-07-23 23:45:55 +0530

    Brodcast app Program

---
+++ Broadcast Webpage.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello I am Home Page"
+
+
+@app.route("/contact")
+def contact():
+    return "Hello I am contact page"
+
+
+@app.route("/aboutus")
+def aboutus():
+    return "Hello I am abutus Page"
+
+
+app.run(port=8081,host='192.168.0.104')  #ip of the machine
